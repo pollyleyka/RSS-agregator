c4caa37ae5ff4990a9a1a40dcff5f4984c56c00f
pollyleyka <polka321@rambler.ru>
2023-09-14 15:48:17 +0300

    add render function

--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,20 @@
 export default (data) => {
   const parser = new DOMParser();
   const newData = parser.parseFromString(data, 'application/xml');
-  console.log(newData);
-  return newData;
-  // console.log(dom.header, dom.feed, dom.items);
-  // return [dom.feed, dom.items];
-  // И вот уже из этого объекта можно достать необходимые данные и вернуть их из парсинга:
-  // заголовок, описание RSS-канала (feed), а также список постов (items)
-  // с их заголовками и описаниями
+  console.log('first', newData);
+  const feedTitle = newData.querySelector('channel title').textContent;
+  const feedDescription = newData.querySelector('channel description').textContent;
+  const feed = {
+    title: feedTitle,
+    description: feedDescription,
+  };
+  const items = newData.querySelectorAll('item');
+  const posts = Array.from(items);
+  posts.map((item) => ({
+    title: item.querySelector('title').textContent,
+    description: item.querySelector('description').textContent,
+    link: item.querySelector('link').textContent,
+  }));
+  console.log('second', feed, posts);
+  return [feed, posts];
 };
